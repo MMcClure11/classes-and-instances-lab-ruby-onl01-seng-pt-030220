45ce8c56ea2b125636c00ca9d4181ebb0e262834
Megan Christine McClure <mcclme02@luther.edu>
2020-03-30 22:46:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
-# class Dog 
+# # class Dog 
+# # end 
+
+# # fido = Dog.new 
+# # snoopy = Dog.new 
+# # lassie = Dog.new 
+
+# class Dog
 # end 
 
 # fido = Dog.new 
 # snoopy = Dog.new 
 # lassie = Dog.new 
-
-class Dog
-end 
-
-fido = Dog.new 
-snoopy = Dog.new 
-lassie = Dog.new 
